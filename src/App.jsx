895ee31bd91c9da0c17fe525010b11ead22eb972
--global <--global>
2024-09-16 15:03:31 -0300

    task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.module.css'
+import { Navbar } from './components/Navbar'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-  
+    <Navbar/>
     </>
   )
 }
