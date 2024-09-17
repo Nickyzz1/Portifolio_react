2115cdff3fce71eab31abe624633d96ff1c65ad0
--global <--global>
2024-09-17 17:16:44 -0300

    analitcs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics/>
+    <SpeedInsights/>
     <App/>
   </StrictMode>,
 )
